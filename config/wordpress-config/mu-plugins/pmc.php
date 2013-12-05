44cdeb082cf4ab0ba9da4498783c900bf0cbc5e7
Hau Vong <hvong@pmc.com>
2013-12-05 15:22:12 -0800

    Only include 411 theme switch if variety411 host is detected

--- config/wordpress-config/mu-plugins/pmc.php
+++ config/wordpress-config/mu-plugins/pmc.php
@@ -10,7 +10,7 @@ add_action( 'muplugins_loaded', function() {
 	}
 } );
 
-if ( file_exists( WP_CONTENT_DIR . "/themes/vip/pmc-411/pmc-411-2013/plugins/theme_switcher.php" ) ) {
+if ( strpos( $_SERVER['HTTP_HOST'], ".variety411." ) !== false  && file_exists( WP_CONTENT_DIR . "/themes/vip/pmc-411/pmc-411-2013/plugins/theme_switcher.php" ) ) {
 	include_once WP_CONTENT_DIR . "/themes/vip/pmc-411/pmc-411-2013/plugins/theme_switcher.php";
 }
 
