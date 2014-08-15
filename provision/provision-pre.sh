1dc9aac4ac42b36220f588319bc2b6180cb97c28
Hau Vong <hvong@pmc.com>
2014-08-15 13:42:15 -0700

    Fix minor bug on pre provision script.

--- provision/provision-pre.sh
+++ provision/provision-pre.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-mysqlshow="$(mysqlshow -uroot -pblank 2>&1)"
-if [[ $mysqlshow != *Access?denied* ]]
+if [[ -f /usr/bin/mysqlshow && -f /usr/bin/mysql ]]
 then
-	echo "Update mysql root password"
-	mysql -uroot -pblank -e " UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root'; FLUSH PRIVILEGES; "
+	mysqlshow="$(mysqlshow -uroot -pblank 2>&1)"
+	if [[ $mysqlshow != *Access?denied* ]]
+	then
+		echo "Update mysql root password"
+		mysql -uroot -pblank -e " UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root'; FLUSH PRIVILEGES; "
+	fi
 fi
